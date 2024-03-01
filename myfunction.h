d488325a9e5fb01db967713e6e79f3e687795388
netrotic <Behzadradmandaan@gmail.com>
2024-03-01 16:33:31 +0330

    cerat testgit

--- myfunction.h
+++ myfunction.h
@@ -5,3 +5,8 @@ void messagePrinter(string str) //This function only prints a certain message
 {
     cout << str << endl;
 }
+
+void testgit(string str)//this is test for behzad
+{
+    cout << str << endl;
+}
